a579a4164277b5212dca639b0091922add1ae158
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-07-06 18:13:50 -0300

    Under construction

---
+++ Módulo 1 -  Lógica de Programação/Projeto final/abertura.py
@@ -0,0 +1,25 @@
+from time import sleep
+# Abretura do jogo
+
+string = '███████╗██╗   ██╗██████╗ ███████╗██████╗    ''██████╗  █████╗ ████████╗ █████╗ ████████╗ █████╗ \n''██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗    ''██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗╚══██╔══╝██╔══██╗\n''███████╗██║   ██║██████╔╝█████╗  ██████╔╝    ''██████╔╝███████║   ██║   ███████║   ██║   ███████║\n''╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗    ''██╔══██╗██╔══██║   ██║   ██╔══██║   ██║   ██╔══██║\n''███████║╚██████╔╝██║     ███████╗██║  ██║    ''██████╔╝██║  ██║   ██║   ██║  ██║   ██║   ██║  ██║\n''╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝    ''╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝'
+def abertura():
+    global string
+    print('=-'*47)
+    print()
+    for i in string: 
+        sleep(0.001) 
+        print(i, end='', flush=True)
+    print()
+    print()
+    print('=-'*47)
+
+    # Introdução ao jogo
+    string = "\nDona Blue comprou uma fazenda e começou a plantar alimentos orgânicos em sua horta.\nAlguns legumes e verduras não ficaram felizes com a suspensão dos agroTÓXICOS e começaram uma revolta!\n\nVocê é uma Super Batata orgânica e deve derrotar os vegetais dependentes químicos arruaceiros.\nMostre que é possível crescer forte e saudável sem depender de agroTÓXICOS.\n\nSó você pode derrotar os vilões e restaurar a ordem na Bluefarm!!!"
+    for i in string: 
+        sleep(0.040) 
+        print(i, end='', flush=True)
+    print()
+    print()
+
+
+abertura()
--- Módulo 1 -  Lógica de Programação/Projeto final/projeto final.py
+++ Módulo 1 -  Lógica de Programação/Projeto final/projeto final.py
@@ -2,20 +2,22 @@ from random import choice
 from time import sleep
 from random import randint
 
+
 class Personagem:
     def __init__(self):
-        forca=10
+        forca=10   #força padrão de qualquer personagem
         self.forca= forca
 
     def escolher_clima(self):
-        self.tempo = ['Chuva','Sol']
-        clima = choice(self.tempo)
-        self.tempo = clima
+        self.clima = ['chovendo','calor']  # clima que atua sobre qualquer personagem
+        clima = choice(self.clima)
+        self.clima = clima
+        return self.clima
        
 class Heroi(Personagem):
     def __init__(self):
         super().__init__()
-        self.vitalidade= 3
+        self.vitalidade= 3 # vida do Herói
 
     def atacar(self,treino=0):
         self.força_adicional = treino
@@ -24,64 +26,123 @@ class Heroi(Personagem):
         dic_treino = {5:'Seus exercícios matutinos foram muito bons e você ganhou +5 de força.',7:
         'Enquanto praticava seus exercícios choveu e você conseguiu ficar muito forte!\nSua força aumentou em +7 pontos.',3:'O de hoje ta pago! hahaha\nVocê ganhou + 3 de força.',2:'Houve um tempo de seca e suas raízes não tiveram de onde puxar nutrientes.\nVocê ganhou +2 de força', 4:'Você começou a treinar mais cedo e conseguiu dobrar seus exercicios!\nVocê ganha +4 de força.'}
         ganho_forca = choice(list(dic_treino.keys()))
-        self.forca += ganho_forca
-        print(dic_treino[ganho_forca]) 
+        
+        if self.clima == 'chovendo':
+            influencia_clima = 3
+            self.forca += (ganho_forca + influencia_clima)
+            print(dic_treino[ganho_forca])
+            print('\n\nA chuva irrigou o solo da Bluefarm durante seus exercícios.\nVocê ganhou +3 de força.!')
+            print(f'\nSua força total agora é: {self.forca}')
+        else:
+            print(dic_treino[ganho_forca]) 
+            
 
 class Vilao(Personagem):
     def __init__(self):
         super().__init__()
-        forca= randint(1,5)
+        ganho_forca= randint(1,5)
+        self.forca += ganho_forca
+         
 
+    def escolher_vilão(self):
+        lista_viloes= ['cenouras','beterrabas','alfaces','tomates','abobrinhas','pepinos','cebolinhas','mandiocas','rabanetes','brócolis']
+        lista_jaForam = list()
+        escolha_vilao = choice[lista_viloes] #Escolhe um vilão da lista_viloes
 
+        while escolha_vilao in lista_jaForam:
+            escolha_vilao = choice[lista_viloes] # Se o vilão escolhido já estiver na lista_jaForam escolhe novamente.
 
-# Abretura do jogo
-print('=-'*47)
-print()
+        lista_jaForam.append(escolha_vilao) # adiciona o vilão escolhido na lista dos vilões que já foram
+        
+        self.escolher_clima()
+        if self.clima == 'calor': #influencia do clima na força do vilão escolhido
+            self.força += 3
+            print(f'\nHouve uma temporada de seca na horta dos arruaceiros, {escolha_vilao} desviaram a irrigação para eles e ganharam +3 de força.')     
+        return escolha_vilao        
+ 
+# Abertura do jogo
 
-string = '███████╗██╗   ██╗██████╗ ███████╗██████╗    ''██████╗  █████╗ ████████╗ █████╗ ████████╗ █████╗ \n''██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗    ''██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗╚══██╔══╝██╔══██╗\n''███████╗██║   ██║██████╔╝█████╗  ██████╔╝    ''██████╔╝███████║   ██║   ███████║   ██║   ███████║\n''╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗    ''██╔══██╗██╔══██║   ██║   ██╔══██║   ██║   ██╔══██║\n''███████║╚██████╔╝██║     ███████╗██║  ██║    ''██████╔╝██║  ██║   ██║   ██║  ██║   ██║   ██║  ██║\n''╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝    ''╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝'
-for i in string: 
-    sleep(0.001) 
-    print(i, end='', flush=True)
-print()
-print()
-print('=-'*47)
-
-# Introdução ao jogo
-string = "\nDona Blue comprou uma fazenda e começou a plantar alimentos orgânicos em sua horta.\nAlguns legumes e verduras não ficaram felizes com a suspensão dos agroTÓXICOS e começaram uma revolta!\n\nVocê é uma Super Batata orgânica e deve derrotar os vegetais dependentes químicos arruaceiros.\nMostre que é possível crescer forte e saudável sem depender de agroTÓXICOS.\n\nSó você pode derrotar os vilões e restaurar a ordem na Bluefarm!!!"
-for i in string: 
-    sleep(0.040) 
-    print(i, end='', flush=True)
-print()
-print()
 
 # Começo do jogo
-
 batata= Heroi()
-print(vars(batata))
+vilao= Vilao()
 
-print('Bom dia, Super Batata!\nÉ um otimo dia para treinar e derrotar os vilões que ameaçam a "BlueFarm". O que deseja fazer?')
-print('1- Treinar')
-print('2- Derrotar alguns legumes arruaceiros')
-escolha=input('')
-while escolha not in ['1','2']:
-     escolha = input('Entrada inválida!\n Digite 1 para treinar ou 2 para enfrentar algum vilão: ')
+climaHeroi = batata.escolher_clima()
 
-if escolha == 2:
-    print('\nOs arruceiros estão plantados a mais tempo que você e se beneficiaram do uso de componentes tóxicos. Tem certeza que deseja ataca-los agora?')
-    print('1- Sim. Eu sou uma Super Batata, irei ataca-los agora mesmo.')
-    print('2-Não, acho melhor treinar um pouquinho antes.')
-    confirma= input('')
-    if confirma == 2:
+print()
+
+if climaHeroi == 'chovendo': # Começo para dias de chuva
+    sleep(1)
+    print(f'Bom dia, Super Batata!')
+    sleep(1.3)
+    print('\nÉ um otimo dia para treinar e derrotar os vilões que ameaçam a "BlueFarm".')
+    sleep(2.5)
+    print('O que deseja fazer?')
+    sleep(1.5)
+    print('\n1- Treinar')
+    sleep(1.3)
+    print('2- Derrotar alguns vegetais arruaceiros')
+    sleep(1.5)
+    print('\nSituação do clima: chovendo')
+    sleep(1)
+    escolha=input('\nR:')
+    while escolha not in ['1','2']:
+        print('Entrada inválida!')
+        sleep(1.2)
+        print('\nDigite 1 para treinar ou 2 para enfrentar algum vilão:')
+        sleep(2)
+        print('Lembre-se, está chovendo!')
+        sleep(1.8)
+        escolha = input('R: ')
+else: # Começo para dias de sol
+    sleep(1)
+    print(f'Bom dia, Super Batata!')
+    sleep(1.3)
+    print('\nÉ um otimo dia para treinar e derrotar os vilões que ameaçam a "BlueFarm".')
+    sleep(2.5)
+    print('O que deseja fazer?')
+    sleep(1.5)
+    print('\n1- Treinar')
+    sleep(1.3)
+    print('2- Derrotar alguns vegetais arruaceiros')
+    sleep(1.5)
+    print('\nSituação do clima: ensolarado')
+    sleep(1)
+    escolha=input('\nR:')
+    while escolha not in ['1','2']:
+        print('Entrada inválida!')
+        sleep(1.2)
+        print('\nDigite 1 para treinar ou 2 para enfrentar algum vilão:')
+        sleep(2)
+        print('Lembre-se, está ensolarado!')
+        sleep(1.8)
+        escolha = input('R: ')
+        sleep(0.5)
+
+# Testando as escolhas 
+if escolha == '2':
+    print('\nOs arruceiros estão plantados a mais tempo que você e se beneficiaram do uso de componentes tóxicos.\nTem certeza que deseja ataca-los?')
+    sleep(5)
+    print('\n1- Sim. Eu sou uma Super Batata, irei ataca-los agora mesmo!')
+    sleep(1.5)
+    print('2- Não, acho melhor treinar mais um pouquinho antes.')
+    escolha= input('R: ')
+    if escolha == '2':
         batata.treinar()
-else:
-    pass
+# Escolhendo inimigo aleatório
+if escolha== '1':
+    inimigos = vilao.escolher_vilão()
+    print(f'Os arruaceiros da vez são {inimigos}.\nSó você, Super Batata, pode derrotar os inimigos e acabar com as raízes do mal! ')
+    
+    # Batalhando
+    if batata.forca < vilao.forca:
+        print(f'{inimigos:.2f}') 
 
 
 
 
-# class Vilão(Personagem):
-#     def __init__(self, força):
-#         super().__init__(força)
-#         self.viloes= ['cenoura','beterrava','alface','tomate','abobrinha','pepino','cebolinha','mandioca','rabanete','brocolis']
+
+
+
 
         
