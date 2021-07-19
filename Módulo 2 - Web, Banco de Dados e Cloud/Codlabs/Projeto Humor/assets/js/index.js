16ecbff475cdd49cc932def0289ee2fd0a963092
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-07-19 10:23:10 -0300

    mudança de nomes do botão

--- Módulo 2 - Web, Banco de Dados e Cloud/Codlabs/Projeto Humor/assets/js/index.js
+++ Módulo 2 - Web, Banco de Dados e Cloud/Codlabs/Projeto Humor/assets/js/index.js
@@ -10,26 +10,31 @@ elementoBtn.addEventListener('click', () =>{
         elementoImg.src = './assets/img/agumon.png' /* Troca a imagem do personagem */
         elementoNome.innerText = 'Agumon' /* Altera o texto do elemento nome */
         elementoSituacao.innerText = 'Nível: criança' /* Altera o texto do elemento situação */
+        elementoBtn.innerText = 'Digivolve para ...'
         elementoBtn.value = 'segundo' /* Altera valor do botão */
     } else if(elementoBtn.value == 'segundo') { /* Verifica se o valour do botão é 'segundo', se for troque de humor para o Dr. Hulk */
         elementoImg.src = './assets/img/graymon.png'  /* Troca a imagem do personagem */
         elementoNome.innerText = 'Graymon' /* Altera o texto do elemento nome */
         elementoSituacao.innerText = 'Nível: adulto' /* Altera o texto do elemento situação */
+        elementoBtn.innerText = 'Digivolve para ...'
         elementoBtn.value = 'terceiro'  /* Altera valor do botão */
     } else if(elementoBtn.value == 'terceiro') { /* Verifica se o valour do botão é 'segundo', se for troque de humor para o Dr. Hulk */
     elementoImg.src = './assets/img/metalgreymon.png'  /* Troca a imagem do personagem */
     elementoNome.innerText = 'MetalGreymon' /* Altera o texto do elemento nome */
     elementoSituacao.innerText = 'Nível: perfeito' /* Altera o texto do elemento situação */
+    elementoBtn.innerText = 'Digivolve para ...'
     elementoBtn.value = 'quarto'  /* Altera valor do botão */
     } else if(elementoBtn.value == 'quarto') { /* Verifica se o valour do botão é 'segundo', se for troque de humor para o Dr. Hulk */
     elementoImg.src = './assets/img/wargreymon.png'  /* Troca a imagem do personagem */
     elementoNome.innerText = 'WarGreymon' /* Altera o texto do elemento nome */
     elementoSituacao.innerText = 'Nível: final' /* Altera o texto do elemento situação */
+    elementoBtn.innerText = 'Voltar'
     elementoBtn.value = 'quinto'  /* Altera valor do botão */
     } else { /* Se os valores do botão não forem, 'primeiro' nem 'segundo' mude para o Dr. Banner */
         elementoImg.src = './assets/img/koromon.png' /* Troca a imagem do personagem */
         elementoNome.innerText = 'Koromon' /* Altera o texto do elemento nome */
         elementoSituacao.innerText = 'Nível: bebê' /* Altera o texto do elemento situação */
+        elementoBtn.innerText = 'Digivolve para ...'
         elementoBtn.value = 'primeiro' /* Altera valor do botão */
     }
 })
